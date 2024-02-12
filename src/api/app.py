11b2d4576d33eaf4d2b6f80b2e6e872dab538147
PrajayVK <prajay827742@gmail.com>
2024-02-12 16:05:43 +0530

    skeleton

--- src/api/app.py
+++ src/api/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def run_script():
     try:
         # Replace 'your_script.py' with the path to your Python script
-        output = subprocess.check_output(['python', 'src/api/test.py'])
+        output = subprocess.check_output(['python', 'P:/PoseDetector/my-vue-app/src/api/test.py'])
         return jsonify(message=output.decode('utf-8'))
     except subprocess.CalledProcessError as e:
         return jsonify(message=str(e))
